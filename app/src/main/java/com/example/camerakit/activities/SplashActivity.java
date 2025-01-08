47076120659dc583f038731c82a0b81296f6d4e8
Muhammad Abbas <g.abbas1992developer@gmail.com>
2025-01-08 17:05:52 +0500

    Created Screens 1. SplashActivity 2. OnBoardingActivity 3. LoginActivity 4. SignUpActivity

---
+++ app/src/main/java/com/example/camerakit/activities/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.camerakit.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.camerakit.R;
+import com.example.camerakit.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
